7b5d7cadf9ab86f59e6399fe607f9f3b4fed8b79
hyungul.roh <knowhunger@gmail.com>
2021-07-26 04:07:10 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Orda Logger
+---------
+Orda Logger is a very tiny logger written in TypeScript, which is used in [Orda-js](https://github.com/orda-io/orda-js)
+project. The size of Orda-logger is about 4KB.
 
-Orda Logger is a very tiny logger written in TypeScript.
+## Install
+
+```shell
+$ npm i @orda-io/orda-logger
+```
+
+## Features
+
+You can use orda-logger as follows:
+
+```typescript
+const logFactory = new OrdaLoggerFactory("trace");
+const logger = logFactory.getLogger("test logging");
+logger.trace("trace message");
+logger.debug("debug message");
+const num = 1234;
+logger.info(`info message:${num}`);
+logger.warn("warn message");
+try {
+  generate_error();
+} catch (e) {
+  logger.error("error message", e);
+}
+logger.log("log withPos back");
+```
+
+- For nodejs, logging messages accompany by the file positions and the tags.
+
+![image](https://user-images.githubusercontent.com/3905310/126910254-c4c1b4cb-e315-4b6f-836e-6316a6dae267.png)
+
+- In the chrome console, logging messages accompany only by the tags.
+
+![image](https://user-images.githubusercontent.com/3905310/126910265-1d7666e4-e9bb-4055-9d74-0567eccfe9f7.png)
--- test/logger.test.ts
+++ test/logger.test.ts
@@ -1,23 +1,39 @@
-import {OrdaLoggerFactory} from "../src/logger";
+import { OrdaLoggerFactory } from "../src";
 
 function generate_error(): Error {
-  throw new Error('generated error');
+  throw new Error("generated error");
 }
 
-describe('Test OrdaLogger', () => {
-  it('Can log with OrdaLogger', () => {
+describe("Test Orda Logger", () => {
+  it("Can log with console logger", () => {
     const num = 1234;
-    const logFactory = new OrdaLoggerFactory('trace');
-    const logger = logFactory.getLogger('test logging');
-    logger.trace('trace message');
-    logger.debug('debug message');
-    logger.info(`log message:${num}`);
-    logger.warn('warn message');
+
+    console.trace("trace message");
+    console.debug("debug message");
+    console.info(`log message:${num}`);
+    console.warn("warn message");
     try {
       generate_error();
     } catch (e) {
-      logger.error('error message', e);
+      console.error("error message", e);
     }
-    logger.log('log withPos back');
+    console.log("log withPos back");
   });
+
+  it("Can log with Orda Logger", () => {
+    const logFactory = new OrdaLoggerFactory("trace");
+    const logger = logFactory.getLogger("test logging");
+    logger.trace("trace message");
+    logger.debug("debug message");
+    const num = 1234;
+    logger.info(`info message:${num}`);
+    logger.warn("warn message");
+    try {
+      generate_error();
+    } catch (e) {
+      logger.error("error message", e);
+    }
+    logger.log("log withPos back");
+  });
+
 });
