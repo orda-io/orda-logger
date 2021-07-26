51c32fa7566df1df6edca7d3b3f33f58cda652f4
hyungul.roh <knowhunger@gmail.com>
2021-07-27 03:20:16 +0900

    remove global variables in browser_or_node.ts

--- src/browser_or_node.ts
+++ src/browser_or_node.ts
@@ -2,8 +2,3 @@ export {isBrowser};
 
 const checkBrowser = new Function('try {return this===window;}catch(e){ return false;}');
 const isBrowser = checkBrowser();
-
-if (!isBrowser) {
-  global.fetch = require('node-fetch');
-  global.WebSocket = require('ws');
-}
